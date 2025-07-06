c9edc92418173490780971bb616be64a02e10fa8
G30TR1X <gavriel10donovan@gmail.com>
2025-07-06 17:41:54 +0700

    Update

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,17 +7,23 @@ public class PlayerScript : MonoBehaviour
 {
     public InputActionReference userInput;
 
-    // Components
+    // 
+    [SerializeField] private TextMeshPro healthText;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform wallCheck;
     [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D playerRigidBody;
+    private GameObject playerChatBubble;
+    private RectTransform playerChatBubbleRect;
+    private TextMeshPro playerChatBubbleText;
 
     // Variables
+    [SerializeField] private int playerHealth = 100;
     [SerializeField] private float playerMovementSpeed = 7f;
     [SerializeField] private float playerJumpPower = 9f;
     [SerializeField] private float playerWallSlidingSpeed = 2f;
+    private bool isInvincible = false;
     private bool isWallSliding = false;
     private bool isWallJumping = false;
     private Vector2 playerDirection;
@@ -24,12 +32,16 @@ public class PlayerScript : MonoBehaviour
     private void Awake()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        playerChatBubble = transform.Find("Bubble").gameObject;
+        playerChatBubbleRect = playerChatBubble.transform.Find("TextBubble").GetComponent<RectTransform>();
+        playerChatBubbleText = playerChatBubble.transform.Find("TextBubble").GetComponent<TextMeshPro>();
+        healthText.SetText(playerHealth.ToString());
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        
+        Debug.Log(playerHealth);
     }
 
     // Update is called once per frame
@@ -41,22 +53,79 @@ public class PlayerScript : MonoBehaviour
         playerWallSliding();
         playerWallJumping();
 
-        if (isOnGround())
-            isWallJumping = false;
-
         if (!isWallSliding)
             playerFaceDirection();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Health"))
+        {
+            playerHealth += 10;
+            Debug.Log(playerHealth);
+            checkPlayerHealth();
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.collider.CompareTag("Spike"))
+        {
+            if (isInvincible)
+                return;
+
+            Debug.Log("Player got hit!");
+            playerHealth -= 25;
+            checkPlayerHealth();
+            playerRigidBody.linearVelocity = new Vector2(wallJumpingDirection * playerMovementSpeed, playerJumpPower);
+            StartCoroutine(invincibility());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Sign"))
+        {
+            playerChatBubbleText.SetText("E");
+            playerChatBubble.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerChatBubble.SetActive(false);
     }
 
     private void playerFaceDirection()
     {
         // Change player direction
         if (playerDirection.x > 0)
+        {
             transform.localScale = new Vector2(1, 1);
+            playerChatBubbleRect.localScale = new Vector2(0.2f, 0.2f);
+        }
         else if (playerDirection.x < 0)
+        {
             transform.localScale = new Vector2(-1, 1);
+            playerChatBubbleRect.localScale = new Vector2(-0.2f, 0.2f);
+        }
 
         wallJumpingDirection = (int) Mathf.Ceil(playerDirection.x) * -1;
+    }
+
+    private void checkPlayerHealth()
+    {
+        if (playerHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (playerHealth > 100)
+        {
+            playerHealth = 100;
+            Debug.Log("Player Health Exceeding!");
+            Debug.Log(playerHealth);
+        }
+
+        healthText.SetText(playerHealth.ToString());
     }
 
     private void playerMove()
@@ -92,9 +161,24 @@ public class PlayerScript : MonoBehaviour
     {
         if (isWallSliding && playerDirection.y > 0)
         {
-            isWallJumping = true;
             playerRigidBody.linearVelocity = new Vector2(wallJumpingDirection * playerMovementSpeed, playerJumpPower);
+
+            StartCoroutine(midairCooldown());
         }
+    }
+
+    private IEnumerator midairCooldown()
+    {
+        isWallJumping = true;
+        yield return new WaitForSeconds(0.25f);
+        isWallJumping = false;
+    }
+
+    private IEnumerator invincibility()
+    {
+        isInvincible = true;
+        yield return new WaitForSeconds(3);
+        isInvincible = false;
     }
 
     private bool isOnGround()
